77bd537f97f0fde41e76eb6bf1b582c56ba1592d
tj-bu-706-07 <tj-bu-706-07@DESKTOP-RLCQ4GQ>
2025-03-11 17:47:08 +0900

    2일차

---
+++ Java/src/Day01/Ex01_Print.java
@@ -0,0 +1,12 @@
+package Day01;
+
+public class Ex01_Print {
+
+	public static void main(String[] args) {
+		System.out.print("안녕하세요");
+		System.out.println("안녕하세요 출력 후 줄바꿈");
+		System.out.println("자바 첫 수업");
+		System.out.println();
+		System.out.println("이어지는 문자열...");
+	}
+}
