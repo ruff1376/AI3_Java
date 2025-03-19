caa4efdb68ca504acf5a23e739bd4a36ac0b8f58
tj-bu-706-07 <tj-bu-706-07@DESKTOP-RLCQ4GQ>
2025-03-19 18:07:27 +0900

    8일차

---
+++ Java/src/Day08/Ex06_UserException/BalanceException.java
@@ -0,0 +1,16 @@
+package Day08.Ex06_UserException;
+
+// 사용자 정의 예외 클래스
+// - public class ??Exception extends Exception { }
+public class BalanceException extends Exception {
+
+	public BalanceException() {
+		super();
+	}
+
+	public BalanceException(String message) {
+		super(message);
+	}
+
+	
+}
