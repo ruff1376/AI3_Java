de8fad2b6007f983b2dc1912d26862af34c82953
tj-bu-706-07 <tj-bu-706-07@DESKTOP-RLCQ4GQ>
2025-03-20 18:20:24 +0900

    9일차 프로그래머스

--- programmers/src/문자열이_몇_번_등장하는지_세기/Solution.java
+++ programmers/src/문자열이_몇_번_등장하는지_세기/Solution.java
@@ -3,12 +3,25 @@ package 문자열이_몇_번_등장하는지_세기;
 public class Solution {
 
 	public static void main(String[] args) {
-		String myString = "";
-		String pat = "";
+		String myString = "banana";
+		String pat = "ana";
+		int answer = 0;
 		for (int i = 0; i < myString.length(); i++) {
+			int i2 = i, cnt = 0;
 			for (int j = 0; j < pat.length(); j++) {
-				
+				if (myString.charAt(i2) == pat.charAt(j)) {
+					i2++;
+					cnt++;
+					if (cnt == pat.length()) {
+						answer++;
+					}
+				}
+				else {
+					break;
+				}
 			}
+			cnt = 0;
 		}
+		System.out.println(answer);
 	}
 }
