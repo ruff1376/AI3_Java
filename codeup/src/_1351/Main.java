73f93244925620bdc59c557f4750c17e1c17823b
tj-bu-706-07 <tj-bu-706-07@DESKTOP-RLCQ4GQ>
2025-03-21 17:18:35 +0900

    10일차

---
+++ codeup/src/_1351/Main.java
@@ -0,0 +1,17 @@
+package _1351;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int s = sc.nextInt(), e = sc.nextInt();
+		for (int i = s; i <= e; i++) {
+			for (int j = 0; j < args.length; j++) {
+				
+			}
+		}
+	}
+
+}
